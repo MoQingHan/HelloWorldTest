4aa2a3e6127966d0dd15b7ab6d09cd3afb955d08
MoQingHan <42321921+MoQingHan@users.noreply.github.com>
2018-08-13 16:09:19 +0800

    添加了查询所有的数据的功能

--- src/main/java/com/sx/test/controller/TestController.java
+++ src/main/java/com/sx/test/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.sx.test.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.sx.test.pojo.Test;
 import com.sx.test.service.TestService;
 
 @Controller
@@ -21,5 +24,16 @@ public class TestController {
 		System.out.println(testService.getCount());
 		return mav;
 	}
-
+	
+	@RequestMapping("/zsbh.do")
+	public ModelAndView getAllZsbh(){
+		ModelAndView mav = new ModelAndView();
+		// 放入jsp路径
+		mav.setViewName(testService.test());
+		List<Test>list=testService.getALlData();
+		for (Test test : list) {
+			System.out.println(test);
+		}
+		return mav;
+	}
 }
